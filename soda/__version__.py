77268de3e2993df50750763b6cca96c3db47a43e
evtn <k01419q45@ya.ru>
2021-05-08 16:45:47 +0700

    added files for setup

---
+++ soda/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of soda package.
+"""
+
+__title__ = 'soda-svg'
+__description__ = 'Fast SVG generation tool'
+__version__ = '0.1.0'
+__author__ = 'evtn'
+__author_email__ = 'g@evtn.ru'
+__license__ = 'MIT License'
+__url__ = 'https://github.com/evtn/soda'
