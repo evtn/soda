173f7cc40ab9947058842aae3f14a3d548316d0d
Dmitry Gritsenko <k01419q45@ya.ru>
2022-11-01 00:13:36 +0400

    Added some tests for Point

--- test/test_point.py
+++ test/test_point.py
@@ -1,3 +1,4 @@
+from math import pi
 from soda import Point
 
 class TestClass:
@@ -12,3 +13,33 @@ class TestClass:
         assert p.x == 2
         assert p.y == 1
         assert p.coords == (2, 1)
+
+    def test_op(self):
+        a = Point(5, 8)
+        b = Point(6, 10)
+
+        assert (a +  b).coords == (a.x +  b.x, a.y +  b.y)
+        assert (a -  b).coords == (a.x -  b.x, a.y -  b.y)
+        assert (a *  b).coords == (a.x *  b.x, a.y *  b.y)
+        assert (a /  b).coords == (a.x /  b.x, a.y /  b.y)
+        assert (a ** b).coords == (a.x ** b.x, a.y ** b.y)
+
+        assert (a +  6).coords == (a.x +  6, a.y +  6)
+        assert (a -  6).coords == (a.x -  6, a.y -  6)
+        assert (a *  6).coords == (a.x *  6, a.y *  6)
+        assert (a /  6).coords == (a.x /  6, a.y /  6)
+        assert (a ** 6).coords == (a.x ** 6, a.y ** 6)
+
+    def test_rotation(self):
+        a = Point(5, 8)
+        
+        assert abs(
+            sum(
+                a.rotate(degrees=90).rotate(degrees=-90)
+            )
+        ) < 1e-10
+
+        assert abs(
+            a.angle(a.rotate(radians=pi / 2)) - pi / 2
+        ) < 1e-10
+
