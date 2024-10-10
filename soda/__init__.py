67554be53f0f23246e8bcb642043f5ab257be988
Dmitry Gritsenko <k01419q45@ya.ru>
2024-10-10 22:07:54 +0400

    fix: import order

--- soda/__init__.py
+++ soda/__init__.py
@@ -1,16 +1,19 @@
+from .tags import Fragment as Fragment
+from .tags import Literal as Literal
+from .tags import Tag as Tag
+
 from .config_mod import config as config
 from .custom_tags import (
     Root as Root,
 )
+
 from .custom_tags import (
     XMLComment as XMLComment,
 )
 from .custom_tags import (
     XMLDeclaration as XMLDeclaration,
 )
+
 from .paths import Path as Path
 from .point import Point as Point
 from .point import PointPath as PointPath
-from .tags import Fragment as Fragment
-from .tags import Literal as Literal
-from .tags import Tag as Tag
